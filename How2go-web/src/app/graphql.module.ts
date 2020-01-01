91a4411d275054cc91aca9851de9b31b26d9933d
Christen <krysten.97@gmail.com>
2020-01-01 11:54:00 +0100

    Récupération des véhicules de l'API et affichage dans module Vehicles

---
+++ How2go-web/src/app/graphql.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+
+//URL de l'api
+const uri = 'https://flow-api.fluctuo.com/v1?access_token=kCumOGxYVctUH1RjilZVZpXBe6OMLMDf';
+
+export function createApollo(httpLink: HttpLink) {
+  return {
+    link: httpLink.create({uri}),
+    cache: new InMemoryCache(),
+  };
+}
+
+@NgModule({
+  exports: [ApolloModule, HttpLinkModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {}
---
+++ How2go-web/src/app/position.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {Apollo} from 'apollo-angular';
+import gql from 'graphql-tag';
+
+export interface LimeBike {
+  long: string;
+  lat: string;
+  batteryLevel: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PositionService {
+
+  currentLat: any;
+  currentLong: any;
+  //requête de base qui renvoie tous les véhicules
+  query = gql`
+          query ($lat: Float!, $lng: Float!) {
+            vehicles (lat: $lat, lng: $lng) {
+              id
+              type
+              attributes
+              lat
+              lng
+              provider {
+                name
+              }
+            }
+          }
+        `;
+
+  constructor(
+    private http: HttpClient,
+    private apollo: Apollo
+    ) { }
+
+  /* FONCTIONS POUR RETROUVER LA POSITION DE L'UTILISATEUR */
+  trackMe() {
+      navigator.geolocation.watchPosition((position) => {
+        this.showTrackingPosition(position);
+      });
+
+  }
+
+  showTrackingPosition(position) {
+    console.log(`tracking postion:  ${position.coords.latitude} - ${position.coords.longitude}`);
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
+  }
+  /******************************/
+
+  //retourne les véhicules
+  getVehicles(): Observable<any> {
+    return this.apollo
+      .watchQuery({
+        query: this.query,
+        variables: {
+          "lat": 48.863341, //c'est ici qu'on renseigne la position que l'on souhaite
+          "lng": 2.335471
+        }
+      })
+      .valueChanges
+  }
+}
