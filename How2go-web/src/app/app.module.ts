d29d37a9f7ac1920399e78f3f80261713fd350da
Bezaliel Marvel <bezalielmarvel@yahoo.com>
2020-01-15 17:51:23 +0100

    initial commit

--- How2go-web/src/app/app.module.ts
+++ How2go-web/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
@@ -18,7 +18,8 @@ import {ReactiveFormsModule} from '@angular/forms';
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
