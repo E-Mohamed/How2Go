b9879a44191a4ef11d903bde27baf97c975a16fb
Bobby Sanchez <ismael.eljarrari@gmail.com>
2020-01-16 00:55:20 +0100

    Add list vehicle

--- How2go-mobile/src/app/components/list/list.component.ts
+++ How2go-mobile/src/app/components/list/list.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { VehicleService } from '../../vehicle.service';
-import { Apollo } from 'apollo-angular';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, Input, OnInit } from '@angular/core';
 import { Vehicle } from '../../models/vehicle.model';
 
 @Component({
@@ -15,27 +11,128 @@ export class ListComponent implements OnInit {
   @Input() vehicles: Vehicle[];
   logo = '../../../assets/images/lime.jpg';
 
-  lime: Vehicle[];
-  velib: Vehicle[];
-  tier: Vehicle[];
-  circ: Vehicle[];
   bird: Vehicle[];
+  bolt: Vehicle[];
+  bMobility: Vehicle[];
+  circ: Vehicle[];
   cityscoot: Vehicle[];
+  dott: Vehicle[];
+  jump: Vehicle[];
+  lime: Vehicle[];
+  mobike: Vehicle[];
+  tier: Vehicle[];
+  velib: Vehicle[];
+  voi: Vehicle[];
+  wind: Vehicle[];
+  
+
+  vehicleName: string[];
 
   constructor() { }
 
   ngOnInit() {
-    this.getLime();
-    this.getBird();
+    this.getBikesByBrand();
+    this.vehicleName = this.getVehicleName();
   }
 
-  getLime() {
-    this.lime = this.filterByName('Lime');
-    console.log(this.lime);
+  getBikesByBrand() {
+    this.getBird();
+    this.getBolt();
+    this.getBMobility();
+    this.getCirc();
+    this.getCityscoot();
+    this.getDott();
+    this.getJump();
+    this.getLime();
+    this.getMobike();
+    this.getTier();
+    this.getVelib();
+    this.getVoi();
+    this.getWind();
   }
-  getBird() {
+
+  getTabByName(name: string): Vehicle[] {
+    switch (name) {
+      case 'Bird':
+        return this.bird;
+        break;
+      case 'Bolt':
+        return this.bolt;
+        break;
+      case 'B Mobility':
+        return this.bMobility;
+        break;
+      case 'Circ':
+        return this.circ;
+        break;
+      case 'Cityscoot':
+        return this.cityscoot;
+        break;
+      case 'Dott':
+        return this.dott;
+        break;
+      case 'Jump':
+        return this.jump;
+        break;
+      case 'Mobike':
+        return this.mobike;
+        break;
+      case 'Tier':
+        return this.tier;
+        break;
+      case 'Velib':
+        return this.velib;
+        break;
+      case 'Voi':
+        return this.voi;
+        break;
+      case 'Wind':
+        return this.wind;
+        break;
+      default:
+        return [];
+        break;
+    }
+  }
+
+  private getBird() {
     this.bird = this.filterByName('Bird');
-    console.log(this.bird);
+  }
+  private getBolt() {
+    this.bolt = this.filterByName('Bolt');
+  }
+  private getBMobility() {
+    this.bMobility = this.filterByName('B Mobility');
+  }
+  private getCirc() {
+    this.circ = this.filterByName('Circ');
+  }
+  private getCityscoot() {
+    this.cityscoot = this.filterByName('Cityscoot');
+  }
+  private getDott() {
+    this.dott = this.filterByName('Dott');
+  }
+  private getJump() {
+    this.jump = this.filterByName('Jump');
+  }
+  private getLime() {
+    this.lime = this.filterByName('Lime');
+  }
+  private getMobike() {
+    this.mobike = this.filterByName('Mobike');
+  }
+  private getTier() {
+    this.tier = this.filterByName('Tier');
+  }
+  private getVelib() {
+    this.velib = this.filterByName('Velib');
+  }
+  private getVoi() {
+    this.voi = this.filterByName('Voi');
+  }
+  private getWind() {
+    this.wind = this.filterByName('Wind');
   }
 
   private filterByName(vehicleName: string): Vehicle[] {
@@ -43,7 +140,15 @@ export class ListComponent implements OnInit {
   }
 
   private getVehicleName(): string[] {
-    let names: string[];
-    return null
+    const names: string[] = [];
+    this.vehicles.forEach(v => {
+      if (!(names.find(n => n === v.provider.name))) {
+        names.push(v.provider.name);
+      }
+    });
+    return names;
   }
+
+
+
 }
