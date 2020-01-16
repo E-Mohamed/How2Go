4001e0b96756be1a91965e0c0b0d998d815884fb
Bobby Sanchez <ismael.eljarrari@gmail.com>
2020-01-16 02:30:18 +0100

    clean code

--- How2go-mobile/src/app/home/home.page.ts
+++ How2go-mobile/src/app/home/home.page.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { Map, tileLayer, marker, icon } from 'leaflet';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { VehicleService } from '../vehicle.service';
-import { Vehicle } from '../models/vehicle.model';
+import { Vehicle } from '../shared/models/vehicle.model';
+import CalculDistance from '../shared/models/CalculDistanceUtils';
 
 @Component({
   selector: 'app-home',
@@ -68,34 +69,8 @@ export class HomePage {
     this.vehicleService.getVehicles(long, lat)
       .subscribe(({ data }) => {
         this.vehicles = data.vehicles;
-        this.distanceCalculator(long, lat);
+        CalculDistance.distanceCalculator(long, lat, this.vehicles);
       });
-  }
-  private distanceCalculator(lon1: number, lat1: number) {
-    for (const vehicle of this.vehicles) {
-      if ((lat1 === vehicle.lat) && (lon1 === vehicle.lng)) {
-        return 0;
-      } else {
-        const radlat1 = Math.PI * lat1 / 180;
-        const radlat2 = Math.PI * vehicle.lat / 180;
-        const theta = lon1 - vehicle.lng;
-        const radtheta = Math.PI * theta / 180;
-        let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        if (dist > 1) {
-          dist = 1;
-        }
-        dist = Math.acos(dist);
-        dist = dist * 180 / Math.PI;
-        dist = dist * 60 * 1.1515 * 1.609344 * 1000;
-
-        vehicle.distance = dist;
-      }
-    }
-    this.orderVehicles();
-  }
-
-  private orderVehicles() {
-    this.vehicles.sort((a: Vehicle, b: Vehicle) => (a.distance > b.distance) ? 1 : -1);
   }
   /** Remove map when we have multiple map object */
   ionViewWillLeave() {
