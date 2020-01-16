d90f73bae996c9c582efc531364fe0c9275aa852
Bobby Sanchez <ismael.eljarrari@gmail.com>
2020-01-16 15:23:06 +0100

    clean code

---
+++ How2go-web/src/app/shared/imagesUrl.ts
@@ -0,0 +1,34 @@
+export const MARKER_ICON = [
+  'https://i.ibb.co/DRJnK3v/birdlogo.png',
+  'bolt-placeholder',
+  'https://i.ibb.co/YN27vmd/bmobilitylogo.png',
+  'https://i.ibb.co/FgDtnky/circlogo.png',
+  'https://multicycles.org/img/cityscoot-2x.48f6bda8.png',
+  'https://i.ibb.co/HX5QQMB/dottlogo.png',
+  'https://i.ibb.co/4VGtjXm/jumplogo.png',
+  'https://multicycles.org/img/mobike-2x.40d9c66f.png',
+  'https://multicycles.org/img/tier-2x.e6d1964e.png',
+  'https://i.ibb.co/sRC0Rcx/veliblogo.png',
+  'https://i.ibb.co/xMBBB7r/voilogo.png',
+  'wind-placeholder'
+];
+
+export const LOGO_ICON = [
+  'https://i.ibb.co/3Y1LKyW/bird.png',
+  'https://i.ibb.co/px2XJTh/bolt.png',
+  'https://i.ibb.co/hfYckKL/boltMob.png',
+  'https://i.ibb.co/hHzQgrL/circ.png',
+  'https://i.ibb.co/r0wk9Nr/cityscoot.png',
+  'https://i.ibb.co/5L3shPP/dott.png',
+  'https://i.ibb.co/cYh8B3r/jump.png',
+  'https://i.ibb.co/0JkpKrf/mobike.png',
+  'https://i.ibb.co/RPcV8LT/tier.png',
+  'https://i.ibb.co/x6KhL5V/velib.png',
+  'https://i.ibb.co/TRfrjt6/voi.png',
+  'https://i.ibb.co/Q6d4ZG8/wind.png'
+];
+
+export const BIKE_NAME = ['Bird', 'Bolt', 'B Mobility', 'Circ', 'Cityscoot', 'Dott', 'Jump', 'Mobike', 'Tier', 'Velib', 'Voi', 'Wind'];
+
+
+
